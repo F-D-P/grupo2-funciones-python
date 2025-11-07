099539da56bf0e2f120f6e96c51522e61ac887e0
MagaliPonce09 <poncerociomagali@gmail.com>
2025-11-07 18:39:17 +0000

    Agrego función ponce

--- tests/test_multiplicacionPonce.py
+++ tests/test_multiplicacionPonce.py
@@ -3,3 +3,4 @@ from funciones.multiplicacionPonce import multiplicar
 def test_multiplicar():
     assert multiplicar(3, 4) == 12
     assert multiplicar(-2, 5) == -10
+    assert multiplicar(0, 14) == 0
